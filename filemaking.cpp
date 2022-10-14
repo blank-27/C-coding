1b80a509164f69c879367143edd2fd472d3f2c2b
bakaboi123 <72279389+bakaboi123@users.noreply.github.com>
2022-10-14 15:26:41 +0530

    added file handling files

---
+++ filemaking.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define sidha(i,x,n) for(ll i=(ll)(x);i<(ll)(n);++i)
+#define ulta(i,x,n) for(ll i=(ll)(x);i>=(ll)(n);--i)
+#define sab(a) a.begin(),a.end()
+#define bas(a) a.rbegin(),a.rend()
+#define test ll _;cin>>_;while(_--)
+#define jaldi ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define INF LLONG_MAX
+//#define MAX 100005
+#define PI (ld)3.141592653589793238
+#define MOD INF
+//#define MOD (ll)1000000007
+//#define MOD (ll)998244353
+
+int main()
+{
+    jaldi;
+    
+    freopen("best.txt", "w", stdout);
+    
+    cout << 1 << '\n';
+
+    // cout << 100 << '\n';
+    // sidha (i, 1, 101) cout << i << ' ';
+    // cout << '\n';
+
+    // cout << 1000 << '\n';
+    // sidha (i, 1, 1001) cout << i << ' ';
+    // cout << '\n';
+
+    // cout << 5000 << '\n';
+    // sidha (i, 1, 5001) cout << i << ' ';
+    // cout << '\n';
+
+    // cout << 10000 << '\n';
+    // sidha (i, 1, 10001) cout << i << ' ';
+    // cout << '\n';
+
+    // cout << 30000 << '\n';
+    // sidha (i, 1, 30001) cout << i << ' ';
+    // cout << '\n';
+
+    cout << 50000 << '\n';
+    sidha (i, 1, 50001) cout << i << ' ';
+    cout << '\n';
+
+    // cout << 70000 << '\n';
+    // sidha (i, 1, 70001) cout << i << ' ';
+    // cout << '\n';
+
+    // cout << 100000 << '\n';
+    // sidha (i, 1, 100001) cout << i << ' ';
+    // cout << '\n';
+
+    // cout << 2000000 << '\n';
+    // sidha (i, 1, 2000001) cout << i << ' ';
+    // cout << '\n';
+
+    return 0;
+}
