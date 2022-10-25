a9bca696006f80a7bfbf8a12baba2b068acae2a3
Nipun Topno <54640890+pigmeister@users.noreply.github.com>
2022-10-26 01:57:25 +0530

    Updated Insertion Sort

--- 123.cpp
+++ 123.cpp
@@ -88,22 +88,25 @@ and so on */
 
 }
 
-void InsertionSort(int arr[]) {
-
-for(int i=1;i<n;i++) {
-int val=arr[i]; // we will find current accurate location of this value in sorted array
-int j=i-1;
-for( ;j>=0;j--) {
-if(val>=arr[j]){
-// means previous element is not more than val so this is exact location of val
-break;
-}
-arr[j+1]=arr[j];
-}
-arr[j+1]=val;
-
-}
-
+void InsertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+ 
+        // Move elements of arr[0..i-1], 
+        // that are greater than key, to one
+        // position ahead of their
+        // current position
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
 }
 
 void BubbleSort(int arr[]) {
