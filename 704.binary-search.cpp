6fb0e19aad8ca0612ac3652d1efe923c257f966a
Yoshikage Kira <56068486+LiGhT-27@users.noreply.github.com>
2023-10-01 16:32:44 +0530

    Update 704.binary-search.cpp

--- 704.binary-search.cpp
+++ 704.binary-search.cpp
@@ -9,6 +9,9 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n=nums.size();
+        for(int i = 0;i < n;i++) {
+            
+        }
     }
 };
 // @lc code=end
